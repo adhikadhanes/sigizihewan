1078319240f475e8581dac8ade7a0b2896c2decc
adhikadhanes <adhika.dhaneswara@gmail.com>
2017-04-08 14:07:24 +0700

    commit

--- resources/views/la/penjualans/index.blade.php
+++ resources/views/la/penjualans/index.blade.php
@@ -65,7 +65,7 @@
 					@la_input($module, 'tgl_penjualan')
 					@la_input($module, 'nama_pembeli')
 					@la_input($module, 'nama_pembeli_retail')
-					@la_input($module, 'tanggal_penerimaan')
+					@la_input($module, 'tanggal_pengiriman')
 					@la_input($module, 'cara_penerimaan')
 					@la_input($module, 'cara_pembayaran')
 					@la_input($module, 'tgl_jatuh_tempo')
